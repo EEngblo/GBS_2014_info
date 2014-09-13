0dec8ab279672fe12fcb9c43a4b6f2b1a492a97c
EEngblo <sheogorath0213@gmail.com>
2014-09-13 21:49:57 +0900

    2253 fix

--- 2253.cpp
+++ 2253.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 
-void main(){
-	int course, a, b;
+int main(){
+	double course, a, b;
 	cin >> course >> a >> b;
 
 	double va = 5.0 / a, vb = 5.0 / b, t;
 	if (course) {
 		if (vb - va == 0) t = 0;
-		else t = abs(5.0 / (vb - va));
+		else t = fabs(5.0 / (vb - va));
 	}
-	else t = abs(5.0 / (vb + va));
+	else t = fabs(5.0 / (vb + va));
 
 	cout << fixed;
 	cout.precision(2);
 	cout << t << endl;
+
+	return 0;
 }
